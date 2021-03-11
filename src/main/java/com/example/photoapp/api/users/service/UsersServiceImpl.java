b2380d54739b22c91d6a756b3be579e53d5d3360
Harish krishnan <harishkrishnan1993@gmail.com>
2021-03-11 19:57:43 +0530

    Add web security config

--- src/main/java/com/example/photoapp/api/users/service/UsersServiceImpl.java
+++ src/main/java/com/example/photoapp/api/users/service/UsersServiceImpl.java
@@ -6,6 +6,7 @@ import com.example.photoapp.api.users.shared.UserDto;
 import org.modelmapper.ModelMapper;
 import org.modelmapper.convention.MatchingStrategies;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import java.util.UUID;
@@ -14,24 +15,28 @@ import java.util.UUID;
 public class UsersServiceImpl implements UsersService {
 
     private UserRepository userRepository;
+    private BCryptPasswordEncoder bCryptPasswordEncoder;
 
     @Autowired
-    public UsersServiceImpl(UserRepository userRepository) {
+    public UsersServiceImpl(UserRepository userRepository, BCryptPasswordEncoder bCryptPasswordEncoder) {
         this.userRepository = userRepository;
+        this.bCryptPasswordEncoder = bCryptPasswordEncoder;
     }
 
     @Override
     public UserDto createUser(UserDto userDetails) {
         userDetails.setUserId(UUID.randomUUID().toString());
+        userDetails.setEncryptedPassword(bCryptPasswordEncoder.encode(userDetails.getPassword()));
 
         ModelMapper modelMapper = new ModelMapper();
         modelMapper.getConfiguration().setMatchingStrategy(MatchingStrategies.STRICT);
 
         UserEntity userEntity = modelMapper.map(userDetails, UserEntity.class);
-        userEntity.setEncryptedPassword("test");
 
         UserEntity savedUser = userRepository.save(userEntity);
 
-        return null;
+        UserDto userDto = modelMapper.map(savedUser, UserDto.class);
+
+        return userDto;
     }
 }
--- src/main/java/com/example/photoapp/api/users/ui/controllers/UsersController.java
+++ src/main/java/com/example/photoapp/api/users/ui/controllers/UsersController.java
@@ -3,10 +3,13 @@ package com.example.photoapp.api.users.ui.controllers;
 import com.example.photoapp.api.users.service.UsersService;
 import com.example.photoapp.api.users.shared.UserDto;
 import com.example.photoapp.api.users.ui.model.CreateUserRequestModel;
+import com.example.photoapp.api.users.ui.model.CreateUserResponseModel;
 import org.modelmapper.ModelMapper;
 import org.modelmapper.convention.MatchingStrategies;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.env.Environment;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
@@ -27,14 +30,16 @@ public class UsersController {
     }
 
     @PostMapping
-    public String createUser(@Valid @RequestBody CreateUserRequestModel userDetails) {
+    public ResponseEntity<CreateUserResponseModel> createUser(@Valid @RequestBody CreateUserRequestModel userDetails) {
         ModelMapper modelMapper = new ModelMapper();
         modelMapper.getConfiguration().setMatchingStrategy(MatchingStrategies.STRICT);
 
         UserDto userDto = modelMapper.map(userDetails, UserDto.class);
 
-        usersService.createUser(userDto);
+        UserDto createdUser = usersService.createUser(userDto);
 
-        return "Create user method is called";
+        CreateUserResponseModel responseModel = modelMapper.map(createdUser, CreateUserResponseModel.class);
+
+        return ResponseEntity.status(HttpStatus.CREATED).body(responseModel);
     }
 }
