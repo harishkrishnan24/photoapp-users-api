e25bda1fb4d69b8d86375dd37787c5326df5033c
Harish krishnan <harishkrishnan1993@gmail.com>
2021-03-11 15:54:37 +0530

    Add users controller

---
+++ src/main/java/com/example/photoapp/api/users/ui/controllers/UsersController.java
@@ -0,0 +1,24 @@
+package com.example.photoapp.api.users.ui.controllers;
+
+import com.example.photoapp.api.users.ui.model.CreateUserRequestModel;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/users")
+public class UsersController {
+
+    @Autowired
+    private Environment environment;
+
+    @GetMapping("/status/check")
+    public String status() {
+        return "Working on port " + environment.getProperty("local.server.port");
+    }
+
+    @PostMapping
+    public String createUser(@RequestBody CreateUserRequestModel userDetails) {
+        return "Create user method is called";
+    }
+}
