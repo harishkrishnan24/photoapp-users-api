438c7b4b911e52cb2f77743fae14815656d2c30e
Harish krishnan <harishkrishnan1993@gmail.com>
2021-03-13 16:08:50 +0530

    Add login feature

--- src/main/java/com/example/photoapp/api/users/service/UsersService.java
+++ src/main/java/com/example/photoapp/api/users/service/UsersService.java
@@ -1,8 +1,10 @@
 package com.example.photoapp.api.users.service;
 
 import com.example.photoapp.api.users.shared.UserDto;
+import org.springframework.security.core.userdetails.UserDetailsService;
 
-public interface UsersService {
+public interface UsersService extends UserDetailsService {
 
     UserDto createUser(UserDto userDetails);
+    UserDto getUserDetailsByEmail(String email);
 }
